74b2c3edac553264ef6b0385e42b8d51b9325b45
Roman Hargrave <roman@hargrave.info>
2016-05-10 16:32:34 -0500

    Add bitkeeper prompt support

---
+++ conf.d/10_bk_prompt_addin.fish
@@ -0,0 +1,21 @@
+# Watch PWD to check for bitkeeper dir entry on dir change
+
+if isatty
+    function -v PWD _bk_prompt.pwd_watcher
+        if bk describe >/dev/null ^/dev/null
+            fish_prompt_addin add _bk_prompt.bk_status_seg
+        else
+            fish_prompt_addin rm _bk_prompt.bk_status_seg
+        end
+    end
+end
+
+function _bk_prompt.bk_status_seg
+    set description (bk describe)
+    if string match -r '.+-dirty' $description >/dev/null
+        set_color red
+    else
+        set_color green
+    end
+    printf 'bk:%s' $description
+end
