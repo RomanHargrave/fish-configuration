1dfa48e82d4bc06095531676ba441da21d14aeca
Roman Hargrave <roman@hargrave.info>
2017-08-01 01:50:29 -0500

    Rename _gen_path_helper to conf._gen_path_helper

--- conf.d/00_config_functions.fish
+++ conf.d/00_config_functions.fish
@@ -1,5 +1,5 @@
 # Shell helpers
-function _gen_path_helper -a name varname
+function conf._gen_path_helper -a name varname
     eval 'function '$name';'\
         'for spec in $argv;'\
             'if not contains $spec $'$varname';'\
@@ -9,9 +9,7 @@ function _gen_path_helper -a name varname
     'end;'
 end
 
-_gen_path_helper conf.padd          PATH
-_gen_path_helper conf.ldadd         LD_LIBRARY_PATH
-_gen_path_helper mark_exportable    fish_export_colons
-_gen_path_helper pkg-config-add     PKG_CONFIG_PATH
-
-functions -e _gen_path_helper
+conf._gen_path_helper conf.padd          PATH
+conf._gen_path_helper conf.ldadd         LD_LIBRARY_PATH
+conf._gen_path_helper mark_exportable    fish_export_colons
+conf._gen_path_helper pkg-config-add     PKG_CONFIG_PATH
