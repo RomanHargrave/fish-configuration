2504563658a97ca3ed3eefef243d3e0943dc6054
Roman Hargrave <roman@hargrave.info>
2020-07-22 23:34:46 -0500

    Reduce fortune set for gentoo

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,5 +1,5 @@
 function fish_greeting -d "what's up, fish?"
-  fortune -u  disclaimer knghtbrd  12% pratchett 18% bofh-excuses 20% off  | cowsay -W 100 -f dragon
+  fortune off bofh-excuses perl   | cowsay -W 100 -f dragon
   set_color $fish_color_autosuggestion[1]
   uname -npsr
   uptime
